e19bffbd979bfef105538e5798f5c86fc5bcf886
kudos2024 <abolajiayobami2000@gmail.com>
2024-09-27 18:45:05 +0100

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { GlobalProvider } from './utils/context.jsx'
+import { GlobalProvider } from './utils/Context.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
